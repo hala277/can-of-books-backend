3a56b74156b48114003c723afc5481b0da26ae8b
hala277 <hala.almasharfeh@gmail.com>
2021-09-29 00:06:34 +0300

    add lab 13

--- modules/books.js
+++ modules/books.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 
 
 const booksSchema = new mongoose.Schema({
-    title:String,
-    description:String,
-    status:String,
-    email:String
+    title: String,
+    description: String,
+    status: String,
+    email: String
 });
 
 
@@ -48,17 +48,87 @@ function sedBooksInfo() {
 // seed only used for one time and its only to add data to mongo db
 // sedBooksInfo();
 // http://localhost:3001/books?email=hala277.almasharfeh277@gmail.com
-function getBooksHandler (request, response){
+function getBooksHandler(request, response) {
     let email2 = request.query.email;
-    booksModel.find({email:email2},function(error,emailData){
-        if(error){
-            console.log('error in getting data',error)
+    booksModel.find({ email: email2 }, function (error, emailData) {
+        if (error) {
+            console.log('error in getting data', error)
         }
-        else{
+        else {
             console.log(emailData)
             response.send(emailData)
         }
     })
 }
 
+// /addBooks?email=${email}&title=${title}&description=${description}&status=${status}
+// /addBooks
+async function addBooksHandler(request, response) {
+    // console.log(request.query);
+    //  the name shold be match at the front and back end
+    //    let {email,title,description,status} = request.query;
+    // all the data when i use post will be in body
+    console.log(request.body);
+
+    let { email, title, description, status } = request.body;
+
+    const newBook = new booksModel({ email, title, description, status })
+    //    I have to try using .then here
+    // I have to take a look for the other way using creat
+    await newBook.save();
+
+    booksModel.find({ email: email }, function (error, emailData) {
+        if (error) {
+            console.log('error in getting data', error)
+        }
+        else {
+            console.log(emailData)
+            response.send(emailData)
+        }
+    })
+}
+
+// /deleteBooks?bookID=${bookID}&email={emailD}
+function deleteBooksHandler(request, response) {
+    let bookID = request.query.bookID;
+    let email = request.query.email;
+
+
+    booksModel.deleteOne({ _id: bookID }).then(() => {
+        booksModel.find({ email: email }, function (error, emailData) {
+            if (error) {
+                console.log('error in getting data', error)
+            }
+            else {
+                // console.log(emailData)
+                response.send(emailData)
+            }
+        })
+    })
+
+
+
+    //    booksModel.remove({_id:bookID},(error,deletedData)=>{
+    //        if(error){
+    //            console.log('error in deleting book data')
+    //        }
+    //        else{
+    //            console.log('deleted data',deletedData)
+    //            booksModel.find({email:email},function(error,emailData){
+    //             if(error){
+    //                 console.log('error in getting data',error)
+    //             }
+    //             else{
+    //                 // console.log(emailData)
+    //                 response.send(emailData)
+    //             }
+    //         })
+    //        }
+    //    })
+}
+
 module.exports = getBooksHandler;
+module.exports = addBooksHandler;
+module.exports = deleteBooksHandler;
+
+
--- server.js
+++ server.js
@@ -8,15 +8,41 @@ require('dotenv').config();
 const server = express();
 server.use(cors());
 const PORT = process.env.PORT;
-
+// middleware to decode any req body to json
+server.use(express.json());
+// const booksModel = mongoose.model('books', booksSchema);
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/books');
 
 const getBooksHandler = require('./modules/books')
+const addBooksHandler = require('./modules/books')
+const deleteBooksHandler = require('./modules/books')
+
+
+
 
 server.get('/', homeHandler);
 server.get('/books',getBooksHandler)
+server.post('/addBooks',addBooksHandler)
+server.delete('/deleteBooks',deleteBooksHandler)
 
+// server.get('/addBooks',addBooksHandler)
+
+
+// // /addBooks?email=${email}&title=${title}&description=${description}&status=${status}
+// function addBooksHandler(request, response){
+//      console.log(request.query);
+//     //  the name shold be match at the front and back end
+//     let {email,title,description,status} = request.query;
+//     const newBook = new booksModel({
+//         email:email,
+//         title: title,
+//         description: description,
+//         status: status,
+      
+//     })
+//     newBook.save();
+// }
 
 function homeHandler(request, response) {
 
